93587da9fe147fcb73f3ede85447efd85dfcc40a
shee-la <sheilayvonne45@gmail.com>
2024-08-22 19:04:30 +0300

    file commit

--- src/App.js
+++ src/App.js
@@ -1,77 +1,13 @@
-import { useState, useEffect } from 'react';
-//import './App.css';
-import SearchIcon from './search.svg';
+import React from 'react';
+import Main from './main';
+import './style.css';
 
-import MovieCard from './MovieCard';
-
-//Here is your key: 405cb58a
-
-//static variable
-const API_URL = 'http://www.omdbapi.com?apikey=405cb58a';
-//const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=405cb58a';
-
-
-const App = () => {
-//creating a state
-const [movies, setMovies] = useState([]);
-const [searchTerm, setSearchTerm] = useState("");
-
-    //fetch data from api above
-       
-        /*calling a function that will fetch the movies*/
-/*accepting search title that one wants to search*/
-        const searchMovies = async (title) =>
-{
-    //call api
-     const response = await fetch(`${API_URL}&S={title}`);
-//once we get the response we have to get the data to a data object
-const data = await response.json(); 
-//output data of the movies
-setMovies(data.Search); //poppulating the movies 
-    }
-
-useEffect (()=>
-{
-// call the search movies function
-searchMovies('spiderman');
-},[] );
-/*empty dependancy array*/ 
-
-    return(
-// JSX application start
-<div className = "App">
-<h1>Movies Site</h1>
-
-<div className = "search"> <input
-placeholder='search for movies'
-value={searchTerm}
-onChange={(e) => setSearchTerm(e.target.value)}
-/> 
-<img
-src={SearchIcon}
-alt="search"
-onClick={()=> searchMovies(searchTerm)}
-/>
-</div>
-
-{
-movies?.length > 0 
-?(
-/*map of the movie*/
-<div className = "container">
-{movies.map((movie)=>(
-    <MovieCard movie={movie}/>
-)
-)} </div>
- ) :
-(
-    <div className ="empty">
-<h2>No movies found</h2>
-    </div>
-)
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
 }
 
-</div>
-    );
-}
-export default App; 
+export default App;
--- src/index.js
+++ src/index.js
@@ -16,13 +16,12 @@
 // //render app to root
 // root.render(<App />);
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
